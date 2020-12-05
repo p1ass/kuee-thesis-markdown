3f46fbe6fc3e03a25d5d255b582aabd8f80ce01d
schktjm <si2.o.mjt@gmail.com>
2020-12-06 03:17:37 +0900

    fix

--- scripts/fetch-dc.sh
+++ scripts/fetch-dc.sh
@@ -2,4 +2,5 @@
 
 studentid=$1
 
-scp -i /github/home/.ssh/fetch_dc_rsa ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
+# scp -i /github/home/.ssh/fetch_dc_rsa ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
+echo "get /usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls" | sftp u-aizu
