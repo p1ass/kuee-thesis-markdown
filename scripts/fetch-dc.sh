168400fc343f14332f9b81f7d49c2eae2e768f18
schktjm <si2.o.mjt@gmail.com>
2020-12-06 00:59:34 +0900

    fix path

--- scripts/fetch-dc.sh
+++ scripts/fetch-dc.sh
@@ -2,4 +2,4 @@
 
 studentid=$1
 
-scp ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive-2016/texmf-local/tex/aizu/U-AizuGT.cls .
+scp ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
