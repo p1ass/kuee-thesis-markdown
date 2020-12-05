81e1555a6c95df8505a7e659a89eeb4dc302b295
schktjm <si2.o.mjt@gmail.com>
2020-12-06 01:08:27 +0900

    fix

--- scripts/fetch-dc.sh
+++ scripts/fetch-dc.sh
@@ -2,4 +2,4 @@
 
 studentid=$1
 
-scp ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
+scp -i ~/.ssh/fetch_dc_rsa ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
