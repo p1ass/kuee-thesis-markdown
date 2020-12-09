9957265393d752168aa3691dee7613067fd44270
schktjm <si2.o.mjt@gmail.com>
2020-12-09 16:38:27 +0900

    setup

--- scripts/fetch-dc.sh
+++ scripts/fetch-dc.sh
@@ -3,4 +3,4 @@
 studentid=$1
 
 # scp -i /github/home/.ssh/fetch_dc_rsa ${studentid}@sshgate.u-aizu.ac.jp:/usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls .
-echo "get /usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls" | sftp -i ~/.ssh/fetch_dc_rsa -oStrictHostKeyChecking=no ${studentid}@sshgate.u-aizu.ac.jp 
+echo "get /usr/local/texlive/texmf-local/tex/aizu/U-AizuGT.cls" | sftp -i secret_key -oStrictHostKeyChecking=no ${studentid}@sshgate.u-aizu.ac.jp 
